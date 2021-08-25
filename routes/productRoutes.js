8f2883fc95f0da1f9efe235569763ff72cc09fd6
g-vicente <g.vicentepresno@gmail.com>
2021-08-25 14:02:02 -0300

    Fotos y Product routes para corregir

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -11,7 +11,7 @@ const checkToken = checkJwt({
 });
 
 productRouter.get("/api/product", productController.index);
-productRouter.get("/api/product/:category", productController.showByCategory);
+productRouter.get("/api/product/cat/:category", productController.showByCategory);
 productRouter.get("/api/product/:slug", productController.show);
 productRouter.post("/api/product", checkToken, productController.store);
 productRouter.patch("/api/product/:id", checkToken, productController.update);
