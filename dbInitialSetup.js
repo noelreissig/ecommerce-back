333499f11c411d420f4c9973b06a009d59ec38a4
g-vicente <g.vicentepresno@gmail.com>
2021-09-02 12:22:18 -0300

    Update dbInitialSetup.js

--- dbInitialSetup.js
+++ dbInitialSetup.js
@@ -7,12 +7,12 @@ module.exports = async (req, res) => {
 	console.log("[Database] ¡Las tablas fueron creadas!");
 
 	// Ejecutar seeders (datos de prueba):
-	await require("./seeders/seederCategory")();
-	await require("./seeders/seederProduct")();
-	await require("./seeders/seederUser")();
-	await require("./seeders/seederAdmin")();
-	await require("./seeders/seederOrder")();
-	await require("./seeders/seederOrder_Product")();
+	// await require("./seeders/seederCategory")();
+	// await require("./seeders/seederProduct")();
+	// await require("./seeders/seederUser")();
+	// await require("./seeders/seederAdmin")();
+	// await require("./seeders/seederOrder")();
+	// await require("./seeders/seederOrder_Product")();
 	// res.status(200).json({ Message: "Error 404 - User not found" });
 	console.log("[Database] ¡Los datos de prueba fueron insertados!");
 };
